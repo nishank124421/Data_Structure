7142ab59c0e49800f51a2143a8e4ed0bcde78ec0
nishank124421 <nishank4643k@gmail.com>
2025-07-08 15:41:23 +0530

    Hello

---
+++ 8th_july.cpp
@@ -0,0 +1,78 @@
+// largest of three numbers//
+// int main(){
+//     int a,b,c;
+//     cout << "Enter the numbers: ";
+//     cin >> a>>b >>c;
+//     if(a>b&&a>c){
+//         cout << "Largest number is: " << a;
+//     }
+//     else if(b>a&&b>c){
+//         cout << "largest number is:" << b;
+//     }
+//     else if (c>a&&c>b){
+//         cout << "largest number is :" << c;
+//     }
+
+// }
+
+// check leap year 
+// int main(){
+//     int year;
+//     cout << "enter the year:";
+//     cin >> year;
+//     if((year%4==0 && year%100!=0) || (year%400==0)){
+//         cout << year << "is a leap year";
+//     }
+//     else 
+//     cout << year << " is not leap year";
+
+// }
+
+// grading system 
+// int main(){
+//     int marks;
+//     cout << "Enter your marks:";
+//     cin >> marks;
+//     if(marks >=90 && marks <=100){
+//         cout << "A";
+//     }
+//     else if(marks >=80 && marks <90){
+//         cout << "B";
+//     }
+//     else if(marks >=70 && marks<80){
+//         cout << "C";
+//     }
+//     else if(marks >=60 && marks<70){
+//         cout << "D";
+//     }
+//     else 
+//     cout << "F";
+// }
+
+//Vowel or constant checker
+// int main(){
+//     char ch;
+//     cout <<"enter a character: ";
+//     cin >> ch;
+//     if(ch =='a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' ||
+//        ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U') {
+//         cout << "Vowel";
+//     }
+//         else cout << "consonant";
+// }
+
+// check if character is alphabet ,digit or special character
+// int main(){
+//     char ch;
+//     cout << "Enter a character";
+//     cin >> ch;
+//     if(ch>='a' && ch <='z' || ch >'A' && ch <='Z'){
+//         cout << "Alphabet";
+//     }
+//     else if(ch>='0' && ch <='9'){
+//         cout << "Digit";
+//     }
+//     else
+//         cout << "Special character";
+
+// }
