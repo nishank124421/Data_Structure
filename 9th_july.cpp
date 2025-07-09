43e958aa91176c5acbd405e71659913c04550271
nishank124421 <nishank4643k@gmail.com>
2025-07-09 15:18:26 +0530

    hello

---
+++ 9th_july.cpp
@@ -0,0 +1,58 @@
+#include<iostream>
+using namespace std;
+// sum of digits of a number using for loop 
+
+
+// int main(){
+//     int n,sum=0;
+//     cout << "Enter the number: ";
+//     cin >> n;
+//     for(int i=n; i>0;i/=10){
+//         sum += i%10;
+//     }
+//     cout << "Sum = " << sum ;
+// }
+
+// Reverse of a number 
+// int main(){
+//     int n, reverse = 0;
+//     cout << "Enter the number: ";
+//     cin >> n;
+//     for(int i = n; i > 0; i /= 10) {
+//         reverse = reverse * 10 + i % 10;
+//     }
+//     cout << "Reverse = " << reverse;
+//     return 0;
+// }
+
+// numbers from 1 to N
+// int main(){
+//     int n;
+//     cout << "Enter the number: ";
+//     cin >> n;
+//     for(int i = 1; i <= n; i++) {
+//         cout << i << " ";
+//     }
+// }
+
+// print multiplication table
+// int main() {
+//     int n;
+//     cout << "Enter the number: ";
+//     cin  >> n;
+//     for(int i=1;i<=10;i++){
+//         cout << n << " * " <<  i  << " = "  <<  n * i << endl;  ;
+//     }
+// }
+
+
+// Factorial of a number
+// int main(){
+//     int n, fact=1;
+//     cout << "Enter the number";
+//     cin >> n;
+//     for(int i=1;i<=n;i++){
+//         fact *=i;
+//     }
+//     cout << "FActorial = " << fact;
+// }
