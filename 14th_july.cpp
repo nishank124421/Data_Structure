a466dd423b2fdeeb3ab499c26f45931b3e6c6349
nishank124421 <nishank4643k@gmail.com>
2025-07-17 11:20:45 +0530

    Initial commit

---
+++ 14th_july.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+// int sum(int x,int y)
+// {
+//     int sum;
+//     return sum=x+y;
+// }
+// int main(){
+//     int x,y;
+//     cin >> x >>y;
+//     cout << sum(x,y);
+// }
+
+// int main(){
+//     cout << sizeof(int) << endl << sizeof(double) << endl <<sizeof(char) << endl<< sizeof(long) << endl << sizeof(float) << endl << sizeof(long long) << endl << sizeof(unsigned);
+
+
+// }
+
